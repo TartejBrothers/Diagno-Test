c11e046dd0da7db7f50892ab654d324cc6ae7a6b
devesh711 <pdevesh699@gmail.com>
2024-02-14 14:45:25 +0530

    add login/signup

--- main/settings.py
+++ main/settings.py
@@ -121,4 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'accounts.User'
