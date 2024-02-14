9cf73aa7d62b0e3c6cb10d5cb15302ece4c348c3
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-02-14 08:45:30 +0530

    Added : Index.html

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import index
+from chat.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
