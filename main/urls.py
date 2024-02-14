ca49df2dd9361ec8c0c03b30adb244db96ee585f
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-02-14 15:18:07 +0530

    Updated: views

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from chat.views import index, chatbot_view
+from chat.oldviews import index, chatbot_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
